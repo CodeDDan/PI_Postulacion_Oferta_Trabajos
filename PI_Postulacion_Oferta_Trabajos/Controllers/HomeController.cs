9b244e3f638d4791925a8b644be18d2227a5b96d
Daniel Sebastian Sánchez Flores <daniel_s2604@hotmail.com>
2024-06-10 18:56:12 -0500

    Agregar archivos de proyecto.

---
+++ PI_Postulacion_Oferta_Trabajos/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PI_Postulacion_Oferta_Trabajos.Models;
+using System.Diagnostics;
+
+namespace PI_Postulacion_Oferta_Trabajos.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PI_Postulacion_Oferta_Trabajos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PI_Postulacion_Oferta_Trabajos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
